5deeb7ec69dcdb8caa9dbc2abab08c51b3d8d0b1
Kay Ousterhout <kay@quilt.io>
2017-06-29 10:19:59 -0700

    docs: Improve error message when formatting fails
    The docs can fail to generate because of mis-formatted code blocks,
    and the existing error message is hard to understand. This commit
    attempts to improve it.

--- lib/multilang.rb
+++ lib/multilang.rb
@@ -1,9 +1,14 @@
 module Multilang
   def block_code(code, full_lang_name)
-    parts = full_lang_name.split('--')
-    rouge_lang_name = parts[0] || ""
-    super(code, rouge_lang_name).sub("highlight #{rouge_lang_name}") do |match|
-      match + " tab-" + full_lang_name
+    begin
+      parts = full_lang_name.split('--')
+      rouge_lang_name = parts[0] || ""
+      super(code, rouge_lang_name).sub("highlight #{rouge_lang_name}") do |match|
+        match + " tab-" + full_lang_name
+    end
+    rescue Exception => e
+      raise "Error reading in block of code. Make sure all code blocks have a language " +
+        "after the opening back quote (e.g., ```javascript). Error: " + e.message
     end
   end
 end
